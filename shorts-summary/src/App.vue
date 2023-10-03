4c1e02fde8fd35de19484d98c77b17b77048eded
Higor Camelo <higorcamelo@gmail.com>
2023-10-03 18:21:52 -0300

    Criação do projeto

---
+++ shorts-summary/src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <v-app>
+    <v-main>
+      <CampoBusca/>
+    </v-main>
+  </v-app>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import CampoBusca from './components/CampoBusca.vue'
+
+export default defineComponent({
+  name: 'App',
+
+  components: {
+    CampoBusca,
+  },
+
+  data () {
+    return {
+      //
+    }
+  },
+})
+</script>
+<style>
+body {
+  font-family: Arial, sans-serif;
+  background-color: #f0f0f0;
+  margin: 0;
+  padding: 0;
+}
+</style>
